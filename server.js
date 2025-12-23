a8e5edd1356e72a6c830625ce7262411d6911372
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2025-12-23 16:31:45 +0900

    form input qo'shildi

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.set("views","views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-    res.end("<h1>Hello world</h1>");
-});
-
-app.get("/gift", function (req, res) {
-    res.end("<p> Your are in gift page</p>");
+app.post("/create-item", (req, res) => {
+      console.log(req.body);
+      res.json({test: "success"});
+})
+app.get('/', function(req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);

4b1eeb3edff377e08a2be380cc21a44fbc33ddd2
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2025-12-25 20:39:29 +0900

    Portfolio

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('started');
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log('ERROR:', err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 // 1: Kirish code 
 app.use(express.static("public"));
 app.use(express.json());
@@ -14,6 +24,9 @@ app.set("views","views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
+app.get('/author', (req, res) => {
+    res.render("author", { user: user });
+})
 app.post("/create-item", (req, res) => {
       console.log(req.body);
       res.json({test: "success"});

a6a7952a6d0681167464204a66f61dcbc7c58a1b
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-01-07 13:22:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongodb.connect(
       module.exports = client
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3000;
+      let PORT = 3003;
       server.listen(PORT, function () {
         console.log(
           `Server is running seccessfully on port: ${PORT} , http://localhost:${PORT}`

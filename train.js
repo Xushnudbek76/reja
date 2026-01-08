d777bdf5e4462214189ad72de33b3ef9440cf012
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-01-08 18:24:25 +0900

    E-Task

--- train.js
+++ train.js
@@ -1,35 +1,4 @@
-class Dokon {
-    constructor(non, lagmon, cola) {
-        this.cola = cola;
-        this.lagmon = lagmon;
-        this.non = non;
-    };
-    time() {
-        const vaqt = new Date();
-        const soat = vaqt.getHours().toString();
-        const daqiqa = vaqt.getMinutes().toString();
-        return `${soat}:${daqiqa}`;
-    }
-
-    qoldiq () {
-        console.log(`${this.time()}da ${this.cola}ta cola ${this.lagmon}ta lagmon va ${this.non}ta non bor`);
-    }
-
-    sotish(mahsulot, soni) {
-        if (this[mahsulot] >= soni) {
-            this[mahsulot] -= soni;
-            console.log(`${soni} ta ${mahsulot} sotildi`)
-        } else {
-         console.log(`${this[mahsulot]} yetarli emas`);            
-        }
-    }
-    qabul(mahsulot, soni) {
-        this[mahsulot] += soni;
-        console.log(`${soni}ta ${mahsulot} qo'shildi`)
-    }
-};
-
-const newDokon = new Dokon(4, 10, 7);
-newDokon.qoldiq();
-newDokon.sotish('cola', 2);
-newDokon.qabul('non', 3);
+const reverse = (str) => {
+    return str.split("").reverse().join('')
+}
+console.log(reverse('wow'));
